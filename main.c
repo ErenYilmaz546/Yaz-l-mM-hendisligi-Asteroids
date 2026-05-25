0454a28413d1ae85f7889ea6078bba3eb22c1b8c
ErenYilmaz546 <eyilmaz372005@gmail.com>
2026-05-26 01:29:54 +0300

    Raylib kuruldu ve oyun penceresi acildi

--- main.c
+++ main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "raylib.h"
 int main()
 {
-    printf("Hello world!\n");
+    //Pencere kurulumu
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    //Pencere olusturma
+    InitWindow(screenWidth, screenHeight, "Astreoids Projesi");
+
+    //fps ayari
+
+    SetTargetFPS(60);
+
+    //ana oyun döngüsü
+    //carpıya veya ESC'ye basılana kadar saniyede 60 kez döner
+
+    while(!WindowShouldClose()){
+
+    //--Güncelleme Kısmıı--
+
+    //--Tus okumalarıve uzay gemisinin hareketi--
+
+    //--Cizim Kısmı
+
+    BeginDrawing();
+
+    //Ekranı her kareden önce tamamen siyah renk ile temizle
+
+    ClearBackground(BLACK);
+
+    //Ekrana test yazısı yazdır
+
+     DrawText("Asteroids Oyununa Hos Geldin!",220,280,20,GREEN);
+
+     EndDrawing();
+    }
+
+    //3.Kapanış ve temizlik
+    //Döngü bitince pencereyi kapat ve belleği boşalt
+
+    CloseWindow();
     return 0;
 }
